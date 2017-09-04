02969e902ff3291b53760c687fcbf35229500e67
KChu <kevinchu1337@gmail.com>
2017-09-04 00:41:11 -0400

    Fixed chart filtering

--- src/main/resources/static/app/chart.controller.js
+++ src/main/resources/static/app/chart.controller.js
@@ -14,10 +14,30 @@
                 chartdata  = response.data;
                 $scope.jobAvgData = {
                     chart: {
-                        "caption": "Highest Paid Jobs",
-                        "subcaption": "Top 10"
+                        caption: "Top 10 Highest Paid Jobs",
+                        numberPrefix: "$"
                     },
                     data: chartdata
                 }
             })
+
+            //creates new chart based on filters
+            $scope.filter = function () {
+                var job = (document.getElementById("jobChart").value);
+                var url = "/top10filtered?job=" + job;
+                var subcaption = job+" jobs"
+                //opens new window to update information as an iframe so url is not displayed
+                var chartPromise = $http.get(url);
+                chartPromise.then(function(response){
+                    chartdata  = response.data;
+                    $scope.jobAvgData = {
+                        chart: {
+                            caption: "Top 10 Highest Paid Jobs",
+                            subcaption: subcaption,
+                            numberPrefix: "$"
+                        },
+                        data: chartdata
+                    }
+            })}
+
         }]);
