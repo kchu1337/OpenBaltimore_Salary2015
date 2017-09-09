3d58b82bc9f5073e096955e50d81b866382f3b26
KChu <kevinchu1337@gmail.com>
2017-09-09 14:25:14 -0400

    Added Testing

--- src/test/java/kchu/datatest/DatatestApplicationTests.java
+++ src/test/java/kchu/datatest/DatatestApplicationTests.java
@@ -1,16 +1,64 @@
 package kchu.datatest;
 
+import kchu.datatest.models.Salary;
+import kchu.datatest.repositories.SalaryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class DatatestApplicationTests {
 
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private SalaryRepository salaryRepository;
+
 	@Test
-	public void contextLoads() {
+	public void dataloader_returns_14017_datasets()
+			throws Exception {
+		mvc.perform(get("/getdata")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$", hasSize(14017)));
+	}
+
+	@Test
+	public void filtering_works_correctly()
+			throws Exception {
+		mvc.perform(get("/top10filtered?job=developer")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content()
+						.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$", hasSize(1)))
+				.andExpect(jsonPath("$[0].label", equalToIgnoringCase("Web Developer")))
+				.andExpect(jsonPath("$[0].value", equalTo(53100.0)));
 	}
 
 }
